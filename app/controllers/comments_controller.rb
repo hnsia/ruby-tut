7699d3ba035f5e788478ddd3105f7d86dee4043c
hnsia <44498178+hnsia@users.noreply.github.com>
2023-04-20 23:01:50 +0800

    Fix typo

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,7 +12,7 @@ class CommentsController < ApplicationController
         @article = Article.find(params[:article_id])
         @comment = @article.comments.find(params[:id])
         @comment.destroy
-        redirect_to article_path(@article), status :see_other
+        redirect_to article_path(@article), status: :see_other
     end
 
     private
